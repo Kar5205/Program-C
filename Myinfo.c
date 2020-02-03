30cfe4f6a56e3d92f7dec422d686e8e646c5394b
Kar5205 <60298522+Kar5205@users.noreply.github.com>
2020-02-03 14:06:31 +0400

    Add files via upload

---
+++ Myinfo.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main(void)
+{
+   int age ,weiht; 
+    char user[50];
+    printf("Իմ անունն է: \n");
+    scanf("%s",user);
+    printf("Իմ տարիքն է: \n");
+    scanf("%d",&age);
+    printf("իմ քաշն է: \n");
+    scanf("%d",&weiht);
+    printf("Իմ տվյալններն է: \n");
+    printf("Անունը:%s\nՏարիքը:%d\nՔաշը:%d\n",user,age,weiht);
+    printf("բարև բոլորին \n");
+}
